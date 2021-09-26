6fda34e80dc45ab17e4ef07f8a2c2ee5c91bb967
blopuszanski <lopuszanski.bartosz@gmail.com>
2021-09-26 15:53:43 +0200

    Add connection with database

--- web_scraping_flat/main.py
+++ web_scraping_flat/main.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup
 from requests import get
 import sqlite3
+from sys import argv
 
 URL = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/mazowieckie/?search%5Bfilter_enum_rooms%5D%5B0%5D=one"
 
 def parse_price(price):
     return float(price.replace(' ', '').replace('zł', '').replace(',','.'))
 
+db = sqlite3.connect('data.db')
+cursor = db.cursor()
+
+if len(argv) > 1 and argv[1] == 'setup':
+    cursor.execute('''CREATE TABLE offers (name TEXT, price REAL, city TEXT)''')
+    quit()
 
 page = get(URL)
 bs = BeautifulSoup(page.content, 'html.parser')
@@ -16,6 +23,7 @@ for offer in bs.find_all('div', class_ = 'offer-wrapper'):
     location = footer.find('small', class_ = 'breadcrumb').get_text().strip().split(',')[0]
     title = offer.find('strong').get_text().strip()
     price = parse_price(offer.find('p', class_ = 'price').get_text().strip())
-    link = offer.find('a')
-    print(link['href'])
-    print(location, title, price)
+
+    cursor.execute('INSERT INTO offers VALUES (?, ?, ?)', (title, price, location))
+
+db.close()
