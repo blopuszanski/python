f4b8da7c449f67841b617e779e53aac3ae241aaa
blopuszanski <lopuszanski.bartosz@gmail.com>
2021-09-26 17:55:01 +0200

    Program is done

--- web_scraping_flat/main.py
+++ web_scraping_flat/main.py
@@ -3,11 +3,24 @@ from requests import get
 import sqlite3
 from sys import argv
 
-URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/mazowieckie/?search%5Bfilter_enum_rooms%5D%5B0%5D=one'
-
 def parse_price(price):
     return float(price.replace(' ', '').replace('zł', '').replace(',','.'))
 
+def parse_page(number):
+    print(f'I am working on the page {number}')
+    page = get(f'{URL}&page={number}')
+    bs = BeautifulSoup(page.content, 'html.parser')
+    for offer in bs.find_all('div', class_='offer-wrapper'):
+        footer = offer.find('td', class_='bottom-cell')
+        location = footer.find('small', class_='breadcrumb').get_text().strip().split(',')[0]
+        title = offer.find('strong').get_text().strip()
+        price = parse_price(offer.find('p', class_='price').get_text().strip())
+
+        cursor.execute("INSERT INTO offers VALUES (?, ?, ?)", (title, price, location))
+
+    db.commit()
+
+URL = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/mazowieckie/?search%5Bfilter_enum_rooms%5D%5B0%5D=one'
 db = sqlite3.connect('flat.db')
 cursor = db.cursor()
 
@@ -15,15 +28,8 @@ if len(argv) > 1 and argv[1] == 'setup':
     cursor.execute("CREATE TABLE offers (name TEXT, price REAL, city TEXT)")
     quit()
 
-page = get(URL)
-bs = BeautifulSoup(page.content, 'html.parser')
-
-for offer in bs.find_all('div', class_ = 'offer-wrapper'):
-    footer = offer.find('td', class_ = 'bottom-cell')
-    location = footer.find('small', class_ = 'breadcrumb').get_text().strip().split(',')[0]
-    title = offer.find('strong').get_text().strip()
-    price = parse_price(offer.find('p', class_ = 'price').get_text().strip())
-    cursor.execute("INSERT INTO offers VALUES (?, ?, ?)", (title, price, location))
-    db.commit()
+l = int(input('Please to input pieces of pages: '))
+for page in range(1,l):
+    parse_page(page)
 
 db.close()
