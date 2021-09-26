3a8bfa89b96e9ee45cf520dca432bdef70d23d81
blopuszanski <lopuszanski.bartosz@gmail.com>
2021-09-26 15:31:25 +0200

    Price values edition

--- web_scraping_flat/main.py
+++ web_scraping_flat/main.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
 from requests import get
 
-URL = "https://www.olx.pl/nieruchomosci/mieszkania/mazowieckie/"
+URL = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/mazowieckie/"
+
+def parse_price(price):
+    return float(price.replace(' ', '').replace('zł', '').replace(',','.'))
+
 
 page = get(URL)
 bs = BeautifulSoup(page.content, 'html.parser')
@@ -9,4 +13,7 @@ bs = BeautifulSoup(page.content, 'html.parser')
 for offer in bs.find_all('div', class_ = 'offer-wrapper'):
     footer = offer.find('td', class_ = 'bottom-cell')
     location = footer.find('small', class_ = 'breadcrumb').get_text().strip().split(',')[0]
-    print(location)
+    title = offer.find('strong').get_text().strip()
+    price = parse_price(offer.find('p', class_ = 'price').get_text().strip())
+
+    print(location, title, price)
